1f5558c7f9082e59eeb03371219f925e8a8dd9f9
Javier Chaler Romero <javichr11@gmail.com>
2024-10-26 09:16:52 +0200

    pruebas

--- BackEnd/src/controllers/eventController.js
+++ BackEnd/src/controllers/eventController.js
@@ -86,7 +86,7 @@ exports.crearEventos = async (req, res) => {
       if (error) {
         return res.status(500).json({ message: 'Error al obtener los eventos', error });
       }
-      return res.status(200).json({message: 'Los eventos se deberían de enviar bien'});
+      return res.status(200).json({message: 'Los eventos se deberían de enviar bien', data});
     }catch(error){
         return res.status(500).json({ message: 'Error del servidor', error });
     }
