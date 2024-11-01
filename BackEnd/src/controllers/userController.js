64bd7fb26d29da686ef5f2e26a45843f72fdf519
Maria Victoria Egido Castillo <MVEGICAS@upv.edu.es>
2024-11-01 14:42:16 +0100

    registro 2.0(?)

--- BackEnd/src/controllers/userController.js
+++ BackEnd/src/controllers/userController.js
@@ -5,23 +5,25 @@ exports.registrarUsuario = async (req, res) => {
 
   const { nombre, edad, movil } = req.body;
 
-  // Verificar que el número de teléfono sea único
-  const { data: existingUser, error: findError } = await supabase
-    .from('usuarios')
-    .select('*')
-    .eq('movil', movil)
-    .single(); 
+  
+  if (movil) {
+    const { data: existingUser, error: findError } = await supabase
+      .from('usuarios')
+      .select('*')
+      .eq('movil', movil)
+      .single();
 
-  if (findError) {
-    return res.status(500).json({ error: "Error al verificar el número de teléfono" });
+    if (findError) {
+      return res.status(500).json({ error: "Error al verificar el número de teléfono" });
+    }
+
+    
+    if (existingUser) {
+      return res.status(400).json({ error: "El número de teléfono ya está registrado" });
+    }
   }
 
-  // Si el número ya está registrado, devuelve un error
-  if (existingUser) {
-    return res.status(400).json({ error: "El número de teléfono ya está registrado" });
-  }
-
-  // Registrar al usuario si el número es único
+  // Registrar al usuario si el número es único o si 'movil' es null
   const { data, error } = await supabase
     .from('usuarios')
     .insert([{ nombre, edad, movil }]);
@@ -33,21 +35,6 @@ exports.registrarUsuario = async (req, res) => {
   res.status(201).json({ message: 'Usuario registrado con éxito', data });
 };
 
-
-exports.obtenerUsuarios = async (req, res) => {
-
-     console.log("Recibido...")
-
-    const { data, error } = await supabase
-      .from('usuarios')
-      .select('*');
-  
-    if (error) {
-      return res.status(400).json({ error: error.message });
-    }
-  
-    res.status(200).json(data);
-  };
 
 
 exports.obtenerUsuarios = async (req, res) => {
