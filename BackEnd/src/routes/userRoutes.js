e2f3ce468cf0dbc1057bd1bca6f0bfd2d2e6756d
Maria Victoria Egido Castillo <MVEGICAS@upv.edu.es>
2024-11-06 00:12:04 +0100

    4

--- BackEnd/src/routes/userRoutes.js
+++ BackEnd/src/routes/userRoutes.js
@@ -7,7 +7,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 // Ruta para registrar un usuario
 //router.post('/registrar', registrarUsuario);
-router.post('/usuario/registrar', upload.single('foto'), userController.registrarUsuario);
+router.post('/registrar', upload.single('foto'), userController.registrarUsuario);
 // Ruta para obtener todos los usuarios
 router.get('/obtener', userController.obtenerUsuarios);
 
