1bfcab56c38c73de5bd8443d59ccd9fd521f54ec
Maria Victoria Egido Castillo <MVEGICAS@upv.edu.es>
2024-11-05 23:18:44 +0100

    funciona 3.0(?)

--- BackEnd/src/routes/userRoutes.js
+++ BackEnd/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { registrarUsuario, obtenerUsuarios } = require('../controllers/userController');
+const upload = require('multer')({ storage: multer.memoryStorage() });
 
 // Ruta para registrar un usuario
-router.post('/registrar', registrarUsuario);
-
+//router.post('/registrar', registrarUsuario);
+router.post('/usuario/registrar', upload.single('foto'), registrarUsuario);
 // Ruta para obtener todos los usuarios
 router.get('/obtener', obtenerUsuarios);
 
