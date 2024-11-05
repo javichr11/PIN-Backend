c213866af01103e72c3cbdfce9224c413fa03164
Maria Victoria Egido Castillo <MVEGICAS@upv.edu.es>
2024-11-05 23:46:54 +0100

    .

--- BackEnd/src/routes/userRoutes.js
+++ BackEnd/src/routes/userRoutes.js
@@ -1,4 +1,4 @@
-const express = require('../config/supabase');
+const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const upload = require('multer')({ storage: multer.memoryStorage() });
