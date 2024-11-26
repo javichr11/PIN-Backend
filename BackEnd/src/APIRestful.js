62f38b8e977b9e01e546a0ac84742174accfcff5
Javier Chaler Romero <javichr11@gmail.com>
2024-11-26 17:12:11 +0100

    de nuevo

--- BackEnd/src/APIRestful.js
+++ BackEnd/src/APIRestful.js
@@ -4,7 +4,6 @@ const usuarioRoutes = require('./routes/userRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 const commentsRoutes = require('./routes/commentsRoutes');
 const ratingRoutes = require('./routes/ratingRoutes');
-const insigniasRoutes = require('./routes/insigniaRoutes');
 
 
 const app = express();
@@ -19,7 +18,6 @@ app.use('/usuario', usuarioRoutes);
 app.use('/evento', eventRoutes);
 app.use('/comentario', commentsRoutes);
 app.use('/valoracion', ratingRoutes);
-app.use('/insignia', insigniaRoutes);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor escuchando en http://0.0.0.0:${PORT}`);
--- BackEnd/src/controllers/insigniasController.js
+++
@@ -1,6 +0,0 @@
-const supabase = require('../config/supabase');
-const { v4: uuidv4 } = require('uuid');
-const multer = require('multer');
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
