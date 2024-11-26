d81bba2fce65e3ae45202e5ce502af8ea9086aa2
Javier Chaler Romero <javichr11@gmail.com>
2024-11-26 16:38:08 +0100

    rutas y controllador

--- BackEnd/src/APIRestful.js
+++ BackEnd/src/APIRestful.js
@@ -18,7 +18,8 @@ app.use(express.json());
 app.use('/usuario', usuarioRoutes);
 app.use('/evento', eventRoutes);
 app.use('/comentario', commentsRoutes);
-app.use('/valoracion', ratingRoutes)
+app.use('/valoracion', ratingRoutes);
+app.use('/insignia', insigniasRoutes);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor escuchando en http://0.0.0.0:${PORT}`);
---
+++ BackEnd/src/controllers/insigniasController.js
@@ -0,0 +1,6 @@
+const supabase = require('../config/supabase');
+const { v4: uuidv4 } = require('uuid');
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
