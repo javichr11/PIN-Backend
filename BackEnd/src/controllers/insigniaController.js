208b963b7ce8b3101b5d685d8e100c5e056e0df9
Javier Chaler Romero <javichr11@gmail.com>
2024-12-10 10:01:30 +0100

    prueba info tabla

--- BackEnd/src/controllers/insigniaController.js
+++ BackEnd/src/controllers/insigniaController.js
@@ -169,7 +169,7 @@ const obtenerLogradas = async (req,res) =>{
         .from('insignias')
         .select('*')
         .innerJoin('insignias_usuario', 'insignias.id = insignias_usuario.insigniaID')
-        .eq('insignias_usuario.userID', userId)
+        .eq('insignias_usuario.userID', userID)
         
     
         if (error) {
