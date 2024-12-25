cb75d02dbde1fdade5250fea9a7f5f869ca4ba65
Javier Chaler Romero <javichr11@gmail.com>
2024-12-26 00:09:41 +0100

    Notificaciones DONE

--- BackEnd/src/APIRestful.js
+++ BackEnd/src/APIRestful.js
@@ -37,6 +37,9 @@ app.post('/test-notifications', async (req, res) => {
 
 async function checkUpcomingEvents(userID) {
   console.log("User recibido: ", userID);
+
+  const now = new Date();
+
   const { data: inscripciones, error } = await supabase
     .from('inscripciones')
     .select(`
