22be58a9a06de94eddf4772dce878d55896a52ea
Javier Chaler Romero <javichr11@gmail.com>
2024-12-28 00:07:45 +0100

    mantener iniciado el perfil

--- BackEnd/src/controllers/userController.js
+++ BackEnd/src/controllers/userController.js
@@ -115,6 +115,7 @@ exports.registrarUsuario = async (req, res) => {
       },
     });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ message: 'Error del servidor', error });
   }
 };
